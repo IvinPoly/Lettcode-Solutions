414d035e4a4f608d594c4c85649638bf19dd8075
Ivin Poly <123885814+IvinPoly@users.noreply.github.com>
2025-02-06 22:18:48 +0530

    Create Anagrams.py
    LeetCode anagrams solutions

---
+++ Anagrams.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+from typing import List
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = defaultdict(list)
+
+        for s in strs:
+            key = tuple(sorted(s))
+            anagrams[key].append(s)
+
+        return list(anagrams.values())
+
+# Example usage
+solution = Solution()
+strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+print(solution.groupAnagrams(strs))
